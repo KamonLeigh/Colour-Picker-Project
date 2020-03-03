160fb426a36556219f3e8b6c04e8865d4c93fa97
Byron Dunkley <KamonLeigh@users.noreply.github.com>
2020-03-03 23:59:25 +0000

    Save palettes to local storage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PaletteList from './PaletteList';
 import SingleColourPalette from './SingleColourPalette';
 import Palette from './Palette';
@@ -11,7 +11,16 @@ import { generatePalette } from './colourHelpers';
 import './App.css';
 
 function App() {
-  const [seedColours, setSeedColours] = useState(seed)
+  const savedPalettes = JSON.parse(window.localStorage.getItem('palettes'));
+  const [seedColours, setSeedColours] = useState(savedPalettes || seed);
+
+
+
+  useEffect(() => {
+  window.localStorage.setItem('palettes', JSON.stringify(seedColours));
+
+  }, [seedColours])
+
   function findPalette(id) {
     return seedColours.find((palette) => {
       return palette.id === id;
@@ -19,8 +28,10 @@ function App() {
   }
 
   function savePalette(newPalette) {
-    setSeedColours([...seedColours, newPalette])
+    setSeedColours([...seedColours, newPalette]);
+  
   }
+
   return (
     <Switch>
       <Route exact path="/palette/new" render={() => <NewPaletteForm savePalette={savePalette} palettes={seedColours}/>}/>
