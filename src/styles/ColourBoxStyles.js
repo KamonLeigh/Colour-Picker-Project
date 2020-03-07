bf58fca9d241668cdc80c2bbdd9c69524a2629e5
Byron Dunkley <KamonLeigh@users.noreply.github.com>
2020-03-07 16:01:31 +0000

    Fixed ColourBox error

--- src/styles/ColourBoxStyles.js
+++ src/styles/ColourBoxStyles.js
@@ -14,14 +14,14 @@ export default {
         "&:hover button": {
             opacity: 1,
         },
-        [sizes.down["lg"]]:{
+        [sizes.down("lg")]:{
             width: "25%",
-            height: props => props.showingFullPalette ? "20%" : "50%",
+            height: props => props.showingFullPalette ? "20%" : "33.33%",
 
         },
-        [sizes.down["md"]]: {
+        [sizes.down("md")]: {
             width: "50%",
-            height: props => props.showingFullPalette ? "10%" : "50%",
+            height: props => props.showingFullPalette ? "10%" : "20%",
 
         },
         [sizes.down("xs")]: {
@@ -117,6 +117,9 @@ export default {
             marginBottom: "0",
             padding: "1rem",
             textTransform: "uppercase",
+            [sizes.down("xs")]: {
+                fontSize: "6rem"
+            }
         },
         "& p": {
             fontSize: "2rem",
